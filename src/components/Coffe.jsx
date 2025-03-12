e2a6fdcee3521251d88938d33bebff579e2c4c62
vanamuthuV <vanamuthuvana22@gmail.com>
2025-03-12 15:53:38 +0530

    responsive routing

--- src/components/Coffe.jsx
+++ src/components/Coffe.jsx
@@ -1,15 +1,30 @@
-import React from 'react'
-import { Link, Outlet } from 'react-router'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Coffe = () => {
-  return (
-      <div>Welcome to Coffee Page
-          <div>
-        <Link to="inside">Click here</Link>
-     
-          </div>
-    </div>
-  )
-}
+  const products = [
+    { id: 1, name: "Cappuccino", price: 3.5 },
+    { id: 2, name: "Espresso", price: 2.5 },
+    { id: 3, name: "Latte", price: 3 },
+    { id: 4, name: "Filter Coffee", price: 3 },
+  ];
 
-export default Coffe
+  return (
+    <div>
+      <h1>Welcome to Coffee Page</h1>
+      <h2>Products</h2>
+      <ul className="space-y-4">
+        {products.map((product) => (
+          <li key={product.id} className="flex justify-between bg-amber-300">
+            <Link to={`/know-more/${product.id}`}>
+              <h3>{product.name}</h3>
+              <p>${product.price}</p>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Coffe;
--- src/routes.jsx
+++ src/routes.jsx
@@ -38,6 +38,12 @@ const routes = [
           {
             path: "",
             element: <Coffe />,
+            children: [
+              {
+                path: ":id",
+                element: <Coffee />,
+              },
+            ],
           },
           {
             path: "inside",
